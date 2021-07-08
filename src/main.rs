6c22a2788b8d94bd40d459717f10398e316914c5
metaborgos <metaborgos@protonmail.com>
2021-07-08 12:01:03 -0400

    adding server

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use messages_actix::MessageApp;
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    let app = MessageApp;
     let app = MessageApp::new(8080);
     app.run()
 }
